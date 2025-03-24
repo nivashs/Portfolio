61d8f640c8feeff87795d1885fc670a9475ef998
Nivash Sudalaimani <154258573+nivashs@users.noreply.github.com>
2025-03-24 17:54:04 +0530

    Ft: Experience and Education

---
+++ src/components/Skills.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+const frontEndSkills = [
+  { name: "Angular", level: 4 },
+  { name: "React", level: 5 },
+  { name: "Nest.js", level: 4 },
+  { name: "Material UI", level: 3 },
+  { name: "Tailwind CSS", level: 4 },
+];
+
+const backEndSkills = [
+  { name: ".Net Core", level: 4 },
+  { name: "Node JS", level: 3 },
+  { name: "FastApi", level: 3 },
+  { name: "PostgreSQL", level: 5 },
+  { name: "Spring Boot", level: 3 },
+];
+
+const tools = [
+  { name: "JavaScript", level: 4 },
+  { name: "C#", level: 4 },
+  { name: "Java", level: 3 },
+  { name: "Python", level: 5 },
+  { name: "Git", level: 5 },
+];
+
+const SkillMeter = ({ level }) => {
+  return (
+    <div className="flex space-x-2">
+      {[...Array(5)].map((_, i) => (
+        <div
+          key={i}
+          className={`h-4 w-8 rounded-full  ${
+            i < level ? "bg-dark" : "bg-gray-300"
+          }`}
+        ></div>
+      ))}
+    </div>
+  );
+};
+const SkillCard = ({ title, skills }) => {
+  return (
+    <div className="p-8 border rounded-xl shadow-lg w-full max-w-2xl">
+      <h2 className="text-xl font-bold mb-6 text-center">{title}</h2>
+      {skills.map((skill, index) => (
+        <div
+          key={index}
+          className="flex justify-between items-center p-4 border-b"
+        >
+          <span className="text-lg font-semibold">{skill.name}</span>
+          <SkillMeter level={skill.level} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const SkillSection = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 justify-center w-full p-8">
+      <SkillCard title="Front-End Development" skills={frontEndSkills} />
+      <SkillCard title="Back-End & Databases" skills={backEndSkills} />
+      <SkillCard title="Programming Languages & Tools" skills={tools} />
+    </div>
+  );
+};
+
+const Skills = () => {
+  return (
+    <>
+      <h2 className="font-bold text-8xl mt-64 mb-16 w-full text-center">
+        Skills
+      </h2>
+      <SkillSection />
+      {/* <div className="w-full h-screen relative flex items-center justify-center rounded-full">
+        <SkillSection />
+      </div> */}
+    </>
+  );
+};
+
+export default Skills;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,110 @@
-import React from "react";
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
+import profilePic from "../../public/images/profile/nivash.jpg";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, motionValue, value]);
+
+  useEffect(() => {
+    springValue.on("change", (latestValue) => {
+      if (ref.current) {
+        ref.current.textContent = Math.ceil(latestValue);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <>
+      <Head>
+        <title>Nivash | About Page</title>
+        <meta name="description" content="nivash" />
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center"></main>
+      <Layout className="pt-16">
+        <AnimatedText text="Dream Build Achieve." className="mb-16" />
+        <div className="grid w-full grid-cols-8 gap-16">
+          <div className="col-span-3 flex flex-col items-start justify-start">
+            <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+              Biography
+            </h2>
+            <p className="font-medium">
+              Hi, I'm Nivash, a Full Stack Developer adept in developing Angular
+              and React based web applications, strong in front-end and back-end
+              technologies. I have 3 years of professional development
+              experience in London Stock Exchange Group
+            </p>
+            <p className="my-4 font-medium">
+              I am always looking for new and innovative ways to bring my
+              clients' visions to life.
+            </p>
+            <p className="font-medium">
+              With my strong interest, knowledge, skills and experience in
+              Software Development, and my positive learning attitude and team
+              spirit, I will bring a great fit to the position to bring in value
+              to the company.
+            </p>
+          </div>
+
+          <div className="col-span-3 relative w-[70%] rounded-2xl border-2 border-solid border-dark bg-light flex items-center justify-center p-4 ml-16">
+            <Image
+              src={profilePic}
+              alt="Nivash"
+              className="h-auto rounded-2xl"
+            />
+          </div>
+          <div className="col-span-2 flex flex-col items-end justify-between">
+            <div className="flex flex-col items-end justify-center">
+              <span className="inline-block text-7xl font-bold">
+                <AnimatedNumbers value={20} />+
+              </span>
+              <h2 className="text-xl font-medium capitalize text-dark/75">
+                satisfied clients
+              </h2>
+            </div>
+            <div className="flex flex-col items-end justify-center">
+              <span className="inline-block text-7xl font-bold">
+                <AnimatedNumbers value={10} />+
+              </span>
+              <h2 className="text-xl font-medium capitalize text-dark/75">
+                projects completed
+              </h2>
+            </div>
+            <div className="flex flex-col items-end justify-center">
+              <span className="inline-block text-7xl font-bold">
+                <AnimatedNumbers value={3} />+
+              </span>
+              <h2 className="text-xl font-medium capitalize text-dark/75">
+                years of experience
+              </h2>
+            </div>
+          </div>
+        </div>
+        <Skills />
+        <Experience />
+        <Education />
+      </Layout>
+    </>
+  );
 };
 
 export default about;
